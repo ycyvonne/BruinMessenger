8b5082b27bfc162164a94397a299433aa018dd00
Yvonne Chen <yc.yvonnechen@yahoo.com>
2016-11-01 23:21:29 -0700

    updated server.js

--- server.js
+++ server.js
@@ -5,20 +5,36 @@ const port = process.env.PORT || 3000
 
 // Set the view engine to handlebars
 app.use(express.static('public'))
+//app.use(bodyParser.urlencoded())
 app.set('view engine', 'hbs')
 
 // TODO: Serve up the login page
 app.get('/', (req, res, next) => {
     // TODO
+    return res.redirect('/login');
+})
+
+app.get('/login', (req, res) => {
+	res.render('login');
 })
 
 // TODO: Serve up the rooms page
 app.get('/rooms', (req, res, next) => {
     // TODO
+    return res.redirect('/rooms');
+})
+
+app.get('/rooms', (req, res) => {
+	res.render('rooms');
 })
 
 // TODO: 404 for all unregistered routes
 app.use((req, res, next) => {
+	return res.redirect('/404');
+})
+
+app.get('/404', (req, res) => {
+	res.render('404');
 })
 
 app.listen(port, () => {
