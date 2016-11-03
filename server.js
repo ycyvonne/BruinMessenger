9230216d26a826b53ded7a4b5cc7da0a3315ab6c
Yvonne Chen <yc.yvonnechen@yahoo.com>
2016-11-03 14:41:10 -0700

    new starter files

--- server.js
+++ server.js
@@ -1,37 +1,23 @@
-// Pull in everything we need to use, including Express
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-// Set the view engine to handlebars
+// set the view engine to handlebars
 app.use(express.static('public'))
-//app.use(bodyParser.urlencoded())
 app.set('view engine', 'hbs')
 
-// TODO: Serve up the login page
 app.get('/', (req, res, next) => {
-    // TODO
-    return res.redirect('/login');
+  res.render('login')
 })
 
-app.get('/login', (req, res) => {
-	res.render('login');
-})
-
-// TODO: Serve up the rooms page
 app.get('/rooms', (req, res, next) => {
-    // TODO
-    res.render('rooms');
+  res.render('rooms')
 })
 
-// TODO: 404 for all unregistered routes
+// 404 for all unregistered routes
 app.use((req, res, next) => {
-	res.render('404');
+  res.status(404).render('404')
 })
-/*
-app.get('/404', (req, res) => {
-	res.render('404');
-})*/
 
 app.listen(port, () => {
   console.log('BruinMessenger running on port: ', port)
